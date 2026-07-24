fe6f75f149750804007e46de6e5e6c90924abbd2
PaliGitHub123 <danyi.pali06@gmail.com>
2026-07-24 10:12:32 +0200

    Initial Commit

---
+++ CouponWallet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CouponWallet
+//
+//  Created by Pál Danyi on 24.07.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CouponWallet/CouponWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponWalletApp.swift
+//  CouponWallet
+//
+//  Created by Pál Danyi on 24.07.26.
+//
+
+import SwiftUI
+
+@main
+struct CouponWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
