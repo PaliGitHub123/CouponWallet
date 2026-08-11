cd2c1da8bef7815d650fa3c3169fee23704c8fb0
PaliGitHub123 <danyi.pali06@gmail.com>
2026-08-11 23:10:13 +0200

    cards styling

---
+++ CouponWallet/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  CouponWallet
+//
+//  Created by Pál Danyi on 11.08.26.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
